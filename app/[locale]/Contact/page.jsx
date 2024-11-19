af13a752d46c5c9d016094e7869365262b615ea9
Abdenour Garti <garti.abdenour@gmail.com>
2024-11-19 12:41:22 +0100

    contactForm v6

--- app/[locale]/Contact/page.jsx
+++ app/[locale]/Contact/page.jsx
@@ -9,14 +9,8 @@ const Contact = () => {
   return (
     <div className='bg-gradient-to-b from-white to-blue-100'>
         <Navbar />
-        <div className="w-full h-[400px] overflow-hidden">
-            <Image 
-                width={400}
-                height={400}
-                src="/contact.jpg" 
-                alt="Contact Background" 
-                className="w-full h-full object-cover"
-            />
+        <div className="w-full h-[400px] bg-[url('/contact.jpg')] bg-cover bg-center overflow-hidden text-white flex items-center justify-center px-[10%] lg:px-[5%]">
+          <h1 className='text-5xl md:text-7xl lg:text-9xl font-bold bg-gradient-to-r from-white to-[#212EA0] text-transparent bg-clip-text'>Contact Us</h1>
         </div>
       <ContactForm />
       <Footer />
